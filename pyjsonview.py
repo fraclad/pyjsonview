23610986a6424cfd5efa2c69e552727a492634bf
Rahmat Ashari <rahmatashari69@gmail.com>
2020-12-31 22:26:13 +0700

    fix order

--- pyjsonview.py
+++ pyjsonview.py
@@ -6,25 +6,42 @@ with open('example_1.json', 'r') as file:
 with open('example_2.json', 'r') as file:
     data1 = json.load(file)
 
-def print_var_name(variable):
+def var_name(variable):
     for name in globals():
         if eval(name) == variable:
             print(name)
-        
-def json_glance(file):
-    print_var_name(file)
+        else:
+            pass
+          
+def json_glance(file, first = True):
+    if first == True:
+        print(var_name(file))
+        global hier 
+        global trace 
+        hier = 0
+        trace = [0]
+    else:
+        hier = hier
     n_master_key = len(file.items())
     for i in range(n_master_key):
         key = list(file.items())[i][0]
         value = list(file.items())[i][1]
-        hier = 0
+        spacer = ' ' * hier
         if isinstance(value, dict) == False:
-            spacer = ' ' * hier
             print(spacer,'|---', str(key))
+            trace.append(0)
         else:
-            spacer = ' ' * hier
             print(spacer,'|---', str(key))
-            hier += 4            
-            json_glance(value)
+            hier += 5
+            json_glance(value, first = False)
+            trace.append(1)
+        if trace[-1] == 1:
+            hier -= 5
+        else:
+            hier = hier
             
 json_glance(data1)
+
+
+
+    
