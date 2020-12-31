2ed031eb6581ca0f906add166b23f1efd6e545ac
Rahmat Ashari <rahmatashari69@gmail.com>
2020-12-31 19:48:27 +0700

    fix data type

--- pyjsonview.py
+++ pyjsonview.py
@@ -2,12 +2,21 @@ import json
 
 with open('example_1.json', 'r') as file:
     data = json.load(file)
+    
+with open('example_2.json', 'r') as file:
+    data1 = json.load(file)
+
 
 def json_glance(file):
-    print('master')
-    for i in range(len(file)):
-        if type(list(file.keys())[i]) != 'dict':
-            print(' |--- ', list(file.keys())[i])
+    print('top')
+    n_master_key = len(file.items())
+    for i in range(n_master_key):
+        key = list(file.items())[i][0]
+        value = list(file.items())[i][1]
+        if isinstance(value, dict) == False:
+            print(' |---', str(key))
+        else:
+            json_glance(value)
             
-json_glance(data)
 
+json_glance(data1)
