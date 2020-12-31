99df39fd4a391e5f0f3063e30ca8b92dea60a48d
Rahmat Ashari <rahmatashari69@gmail.com>
2020-12-31 20:13:20 +0700

    add var name func

--- pyjsonview.py
+++ pyjsonview.py
@@ -6,17 +6,25 @@ with open('example_1.json', 'r') as file:
 with open('example_2.json', 'r') as file:
     data1 = json.load(file)
 
-
+def print_var_name(variable):
+    for name in globals():
+        if eval(name) == variable:
+            print(name)
+        
 def json_glance(file):
-    print('top')
+    print_var_name(file)
     n_master_key = len(file.items())
     for i in range(n_master_key):
         key = list(file.items())[i][0]
         value = list(file.items())[i][1]
+        hier = 0
         if isinstance(value, dict) == False:
-            print(' |---', str(key))
+            spacer = ' ' * hier
+            print(spacer,'|---', str(key))
         else:
+            spacer = ' ' * hier
+            print(spacer,'|---', str(key))
+            hier += 4            
             json_glance(value)
             
-
 json_glance(data1)
